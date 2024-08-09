80de7e3d7debbfc99cca0a95f4c5e26ae3687347
Viraj Gudhetwar <virajg0246@gmail.com>
2024-08-09 16:46:34 +0530

    Restart

--- script.js
+++
@@ -1,7 +0,0 @@
-let newBtn = document.createElement("button");
-newBtn.innerText = "click me!"
-
-newBtn.style.color = "white";
-newBtn.style.backgroundColor = "red";
-
-document.querySelector("body").prepend(newBtn);
